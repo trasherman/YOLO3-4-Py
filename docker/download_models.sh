d0d29743df8e318ff136d6c78a493a6ffff71bb6
trasherman <64957742+trasherman@users.noreply.github.com>
2020-05-09 15:01:57 +0200

    Update download_models.sh

--- docker/download_models.sh
+++ docker/download_models.sh
@@ -6,11 +6,6 @@ mkdir cfg
 wget -O cfg/coco.data https://trasherman.imfast.io/darknet/coco.data
 wget -O cfg/yolov3.cfg https://trasherman.imfast.io/darknet/yolov3.cfg
 
-echo "Modify config parameters to enable Testing mode"
-sed -i '/batch=64/c\# batch=64' cfg/yolov3.cfg
-sed -i '/subdivisions=16/c\# subdivisions=16' cfg/yolov3.cfg
-sed -i '/# batch=1/c\batch=1' cfg/yolov3.cfg
-sed -i '/# subdivisions=1/c\subdivisions=1' cfg/yolov3.cfg
 
 mkdir data
 wget -O data/coco.names https://trasherman.imfast.io/darknet/coco.names
