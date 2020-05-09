bdf8380666896e0c9ea352c45f21afb545a23597
trasherman <64957742+trasherman@users.noreply.github.com>
2020-05-09 12:29:14 +0200

    Create download_sample_images.sh

---
+++ input/download_sample_images.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Image courtesy Absolute Free Photos. Licensed under Creative Commons Zero (CC0) License.
+
+wget https://www.olympicpartyhire.com.au/imager/assets/volumes/product-images/11318/Giant-Playing-Cards-Olympic-Party-Hire-Adelaide_bd78932829a64e340667602cd6642751.jpg
